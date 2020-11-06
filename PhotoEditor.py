c54173c74b6b18648875d97688333aa5a4858865
nekabr <q123dbb-r@ya.ru>
2020-11-06 16:23:21 +0300

    add ability to upload file, but found a problem with all buttons

--- PhotoEditor.py
+++ PhotoEditor.py
@@ -1,10 +1,9 @@
 import sys
-
 from PyQt5 import uic
-from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 
 Picture_SIZE = [400, 400]
 
@@ -13,14 +12,23 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('supadesign.ui', self)
+        self.current = 'newFile.png'
+        self.pixmap = QPixmap(self.current)
+        self.image = QLabel(self)
+        self.image.resize(700, 700)
+        self.image.setPixmap(self.pixmap)
         self.actionOpen.triggered.connect(self.openFile)
         self.actionNew.triggered.connect(self.newFile)
 
-
     def openFile(self):
+        filename = QFileDialog.getOpenFileName(self, 'Выберите картинку', '')[0]
+        self.pixmap = QPixmap(filename)
+        self.image.setPixmap(self.pixmap)
+        self.image = QLabel(self)
+        self.image.resize(700, 700)
 
     def newFile(self):
-
+        pass
 
 
 if __name__ == '__main__':
