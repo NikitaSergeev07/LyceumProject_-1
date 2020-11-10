5d68df8ae10383baf3b21a7e3c24f3b109391821
nekabr <q123dbb-r@ya.ru>
2020-11-10 16:56:08 +0300

    added some functions

--- PhotoEditor.py
+++ PhotoEditor.py
@@ -1,26 +1,19 @@
-import sys
 import io
+import sys
+from PIL import *
 from PyQt5 import uic
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import *
-from PIL import Image
-
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('supadesign.ui', self)
-        self.current = 'newFile.png'
-        self.pixmap = QPixmap(self.current)
-        self.image = QLabel(self)
-        self.image.move(0, 23)
-        img = Image.open(self.current)
-        x, y = img.size
-        self.image.resize(x, y)
-        self.image.setPixmap(self.pixmap)
+        self.Slider.setVisible(False)
+        self.freeimg = "freeFile.png"
         self.actionOpen.triggered.connect(self.openFile)
         self.actionNew.triggered.connect(self.newFile)
         self.actionJust_Red.triggered.connect(self.justRed)
@@ -29,37 +22,95 @@ class MyWidget(QMainWindow):
         self.filename = QFileDialog.getOpenFileName(self, 'Выберите картинку', '')[0]
         self.pixmap = QPixmap(self.filename)
         self.image.setPixmap(self.pixmap)
-        self.image = QLabel(self)
-        self.image.move(0, 23)
+        img = QImage(self.filename)
+        buffer = QBuffer()
+        buffer.open(QBuffer.ReadWrite)
+        img.save(buffer, "PNG")
+        pil_img = Image.open(io.BytesIO(buffer.data()))
+        x, y = pil_img.size
+        self.image.resize(x, y)
 
     def newFile(self):
-        pass
+        self.current = 'newFile.png'
+        self.pixmap = QPixmap(self.current)
+        img = Image.open(self.current)
+        x, y = img.size
+        self.image.resize(x, y)
+        self.image.setPixmap(self.pixmap)
+        self.filename = self.current
 
     def justRed(self):
         img = QImage(self.filename)
         buffer = QBuffer()
-        buffer.open(QIODevice.ReadWrite)
+        buffer.open(QBuffer.ReadWrite)
         img.save(buffer, "PNG")
+        pil_img = Image.open(io.BytesIO(buffer.data()))
+        pixels = pil_img.load()
+        x, y = pil_img.size
+        for i in range(x):
+            for j in range(y):
+                r, g, b = pixels[i, j]
+                pixels[i, j] = r, 0, 0
+        img.save(self.freeimg)
+        self.pixmap = QPixmap(self.freeimg)
+        self.image.setPixmap(self.pixmap)
 
-        strio = cStringIO.StringIO()
-        strio.write(buffer.data())
-        buffer.close()
-        strio.seek(0)
-        pil_img = Image.open(strio)
+    def justGreen(self):
+        img = QImage(self.filename)
+        buffer = QBuffer()
+        buffer.open(QBuffer.ReadWrite)
+        img.save(buffer, "PNG")
+        pil_img = Image.open(io.BytesIO(buffer.data()))
         pixels = pil_img.load()
         x, y = pil_img.size
         for i in range(x):
             for j in range(y):
                 r, g, b = pixels[i, j]
                 pixels[i, j] = 0, g, 0
-        img.save(self.new_img)
-        self.pixmap = QPixmap(self.new_img)
+        img.save(self.freeimg)
+        self.pixmap = QPixmap(self.freeimg)
         self.image.setPixmap(self.pixmap)
 
+    def justBlue(self):
+        img = QImage(self.filename)
+        buffer = QBuffer()
+        buffer.open(QBuffer.ReadWrite)
+        img.save(buffer, "PNG")
+        pil_img = Image.open(io.BytesIO(buffer.data()))
+        pixels = pil_img.load()
+        x, y = pil_img.size
+        for i in range(x):
+            for j in range(y):
+                r, g, b = pixels[i, j]
+                pixels[i, j] = 0, 0, b
+        img.save(self.freeimg)
+        self.pixmap = QPixmap(self.freeimg)
+        self.image.setPixmap(self.pixmap)
+
+    def Negative(self):
+        img = QImage(self.filename)
+        buffer = QBuffer()
+        buffer.open(QBuffer.ReadWrite)
+        img.save(buffer, "PNG")
+        pil_img = Image.open(io.BytesIO(buffer.data()))
+        pixels = pil_img.load()
+        x, y = pil_img.size
+        for i in range(x):
+            for j in range(y):
+                r, g, b = pixels[i, j]
+                pixels[i, j] = r, 0, 0
+        img.save(self.freeimg)
+        self.pixmap = QPixmap(self.freeimg)
+        self.image.setPixmap(self.pixmap)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+    form = MyWidget()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
