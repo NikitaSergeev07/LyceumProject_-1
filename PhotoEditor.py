25209e717543ec9080fedc6157a6422e4e7e6b7e
nekabr <q123dbb-r@ya.ru>
2020-11-12 04:55:15 +0300

    Time to stop this night, you can't do anything else now...

--- PhotoEditor.py
+++ PhotoEditor.py
@@ -10,8 +10,10 @@ from Design_PhotoProcessing import Ui_MainWindow
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setGeometry(100, 100, 500, 200)
         self.coefficient = 0
         self.setupUi(self)
+        self.Slider.setVisible(False)
         self.image.setPixmap(QPixmap('background.png'))
         self.actionOpen.triggered.connect(self.openFile)
         self.actionNew.triggered.connect(self.newFile)
@@ -22,10 +24,19 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.All.triggered.connect(self.all)
         self.actionCurvies.triggered.connect(self.Inbright)
         self.BnW.triggered.connect(self.blwh)
-        self.Sepia.triggered.connect(self.sepiit)
-        self.actionGray_Skale.triggered.connect(self.gray_scale)
-        self.actionContrast.triggered.connect(self.contrast)
-        self.actionSquare_2.triggered.connect(self.crop_square)
+        self.Sepia.triggered.connect(self.sepi_it)
+        self.gray_skale.triggered.connect(self.gray_scale)
+        self.acontrast.triggered.connect(self.contrast)
+        self.a11.triggered.connect(self.crop_square)
+        self.a169.triggered.connect(self.crop_rect)
+        self.a34.triggered.connect(self.crop_r34)
+        self.a43.triggered.connect(self.crop_r43)
+        self.a23.triggered.connect(self.crop_r23)
+        self.a32.triggered.connect(self.crop_r32)
+        self.actionLeft.triggered.connect(self.rotate_left)
+        self.actionRight.triggered.connect(self.rotate_right)
+        self.actionFlip_from_right_to_left.triggered.connect(self.flip_hor)
+        self.actionFlip_vertically.triggered.connect(self.flip_vert)
 
     def openFile(self):
         self.filename = QFileDialog.getOpenFileName(self, 'Выберите картинку', '')[0]
@@ -34,6 +45,8 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.x, self.y = self.mainimg.size
         self.pic.save('freeFile.png')
         self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(self.x, self.y)
+        self.setGeometry(100, 100, self.x + 20, self.y + 20)
 
     def newFile(self):
         self.filename = 'newFile.png'
@@ -103,21 +116,22 @@ class MyWidget(QMainWindow, Ui_MainWindow):
     def blwh(self):
         brightness, ok = QInputDialog.getInt(self, "Input Brightness", 'What brightness do you want to use?', 2, 1, 15,
                                              1)
-        separator = 255 / brightness / 2 * 3
-        self.pic = self.mainimg.copy()
-        pixels = self.pic.load()
-        for i in range(self.x):
-            for j in range(self.y):
-                r, g, b = pixels[i, j]
-                total = r + g + b
-                if total > separator:
-                    self.pic.putpixel((i, j), (255, 255, 255))
-                else:
-                    self.pic.putpixel((i, j), (0, 0, 0))
-        self.pic.save('freeFile.png')
-        self.image.setPixmap(QPixmap('freeFile.png'))
+        if ok:
+            separator = 255 / brightness / 2 * 3
+            self.pic = self.mainimg.copy()
+            pixels = self.pic.load()
+            for i in range(self.x):
+                for j in range(self.y):
+                    r, g, b = pixels[i, j]
+                    total = r + g + b
+                    if total > separator:
+                        self.pic.putpixel((i, j), (255, 255, 255))
+                    else:
+                        self.pic.putpixel((i, j), (0, 0, 0))
+            self.pic.save('freeFile.png')
+            self.image.setPixmap(QPixmap('freeFile.png'))
 
-    def sepiit(self):
+    def sepi_it(self):
         self.pic = self.mainimg.copy()
         pixels = self.pic.load()
         for i in range(self.x):
@@ -177,9 +191,110 @@ class MyWidget(QMainWindow, Ui_MainWindow):
 
     def crop_square(self):
         self.pic = self.mainimg.copy()
-        self.pic = self.pic.crop((177, 882, 1179, 1707))
-        self.pic.save('freeFile.png')
+        x1 = int(self.x / 2 - self.y / 2)
+        y1 = int(self.x / 2 + self.y / 2)
+        pic2 = self.pic.crop((x1, 0, y1, self.y))
+        pic2.save('freeFile.png')
+        x, y = pic2.size
         self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def crop_rect(self):
+        self.pic = self.mainimg.copy()
+        if int(self.x / self.y) > int(16 / 9):
+            x1 = int(self.x / 2 - 8 * self.y / 9)
+            y1 = int(self.x / 2 + 8 * self.y / 9)
+            pic2 = self.pic.crop((x1, 0, y1, 0))
+        elif int(self.x / self.y) < int(16 / 9):
+            x1 = int(self.y / 2 - 9 * self.x / 32)
+            y1 = int(self.y / 2 + 9 * self.x / 32)
+            pic2 = self.pic.crop((0, x1, 0, y1))
+        else:
+            pic2 = self.pic.copy()
+        pic2.save('freeFile.png')
+        x, y = pic2.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def crop_r34(self):
+        self.pic = self.mainimg.copy()
+        new_size = (1795, 2398)
+        ratio = min(float(new_size[0]) / self.x, float(new_size[1]) / self.y)
+        x1 = int(self.x * ratio)
+        y1 = int(self.y * ratio)
+        pic2 = self.pic.crop((0, 0, x1, y1))
+        pic2.save('freeFile.png')
+        x, y = pic2.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def crop_r43(self):
+        self.pic = self.mainimg.copy()
+        new_size = (2398, 1795)
+        ratio = min(float(new_size[0]) / self.x, float(new_size[1]) / self.y)
+        x1 = int(self.x * ratio)
+        y1 = int(self.y * ratio)
+        pic2 = self.pic.crop((0, 0, x1, y1))
+        pic2.save('freeFile.png')
+        x, y = pic2.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def crop_r23(self):
+        self.pic = self.mainimg.copy()
+        new_size = (1205, 1795)
+        ratio = min(float(new_size[0]) / self.x, float(new_size[1]) / self.y)
+        x1 = int(self.x * ratio)
+        y1 = int(self.y * ratio)
+        pic2 = self.pic.crop((0, 0, x1, y1))
+        pic2.save('freeFile.png')
+        x, y = pic2.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def crop_r32(self):
+        self.pic = self.mainimg.copy()
+        new_size = (1795, 1205)
+        ratio = min(float(new_size[0]) / self.x, float(new_size[1]) / self.y)
+        x1 = int(self.x * ratio)
+        y1 = int(self.y * ratio)
+        pic2 = self.pic.crop((0, 0, x1, y1))
+        pic2.save('freeFile.png')
+        x, y = pic2.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def rotate_left(self):
+        self.pic = self.mainimg.copy()
+        self.pic = self.pic.transpose(Image.ROTATE_90)
+        self.pic.save('freeFile.png')
+        x, y = self.pic.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def rotate_right(self):
+        self.pic = self.mainimg.copy()
+        self.pic = self.pic.transpose(Image.ROTATE_270)
+        self.pic.save('freeFile.png')
+        x, y = self.pic.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def flip_vert(self):
+        self.pic = self.mainimg.copy()
+        self.pic = self.pic.transpose(Image.FLIP_LEFT_RIGHT)
+        self.pic.save('freeFile.png')
+        x, y = self.pic.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
+
+    def flip_hor(self):
+        self.pic = self.mainimg.copy()
+        self.pic = self.pic.transpose(Image.FLIP_TOP_BOTTOM)
+        self.pic.save('freeFile.png')
+        x, y = self.pic.size
+        self.image.setPixmap(QPixmap('freeFile.png'))
+        self.image.resize(x, y)
 
 
 def curve(pixel):
